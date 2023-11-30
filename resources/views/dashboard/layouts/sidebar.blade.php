454964aff19e17cd4943d7d92d790adac05d4526
TajulMunandar <tajulmunandar701@gmail.com>
2023-11-30 11:51:33 +0700

    hak akses sidebar

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -9,77 +9,71 @@
 
     <ul class="menu-inner py-1">
         {{-- Dashboard --}}
-        <li class="menu-item {{ Request::is('dashboard') ? 'active' : '' }}">
-            <a href="/dashboard" class="menu-link">
-                <i class="fa-duotone fa-grid-2 me-3"></i>
-                <div data-i18n="Analytics">Dashboard</div>
-            </a>
-        </li>
+        @if (auth()->user()->isAdmin == 0)
+            <li class="menu-item {{ Request::is('dashboard') ? 'active' : '' }}">
+                <a href="/dashboard" class="menu-link">
+                    <i class="fa-duotone fa-grid-2 me-3"></i>
+                    <div data-i18n="Analytics">Dashboard</div>
+                </a>
+            </li>
 
-        {{-- Perangkat Gampong --}}
-        {{-- <li class="menu-item {{ Request::is('dashboard/struktur*') ? 'active open' : '' }}">
-            <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="fa-duotone fa-list-tree me-3"></i>
-                <div data-i18n="Layouts">Struktur Pengurus</div>
-            </a>
+            <li class="menu-item {{ Request::is('dashboard/sppd*') ? 'active' : '' }}">
+                <a href="/dashboard/sppd" class="menu-link">
+                    <i class="fa-duotone fa-note-sticky fa-sm me-3"></i>
+                    <div data-i18n="Analytics">SPPD</div>
+                </a>
+            </li>
+        @else
+            <li class="menu-item {{ Request::is('dashboard') ? 'active' : '' }}">
+                <a href="/dashboard" class="menu-link">
+                    <i class="fa-duotone fa-grid-2 me-3"></i>
+                    <div data-i18n="Analytics">Dashboard</div>
+                </a>
+            </li>
+            <li class="menu-item {{ Request::is('dashboard/sppd*') ? 'active' : '' }}">
+                <a href="/dashboard/sppd" class="menu-link">
+                    <i class="fa-duotone fa-note-sticky fa-sm me-3"></i>
+                    <div data-i18n="Analytics">SPPD</div>
+                </a>
+            </li>
 
-            <ul class="menu-sub">
-                <li class="menu-item {{ Request::is('dashboard/struktur/perangkat-gampong*') ? 'active' : '' }}">
-                    <a href="/dashboard/struktur/perangkat-gampong" class="menu-link">
-                        <div data-i18n="Analytics">Perangkat Gampong</div>
-                    </a>
-                </li>
-                <li class="menu-item {{ Request::is('dashboard/struktur/jabatan*') ? 'active' : '' }}">
-                    <a href="/dashboard/struktur/jabatan" class="menu-link">
-                        <div data-i18n="data surat">Kelola Jabatan</div>
-                    </a>
-                </li>
-            </ul>
-        </li> --}}
+            <li class="menu-item {{ Request::is('dashboard/pegawai*') ? 'active' : '' }}">
+                <a href="/dashboard/pegawai" class="menu-link">
+                    <i class="fa-duotone fa-user-tie fa-sm me-3"></i>
+                    <div data-i18n="Analytics">Pegawai</div>
+                </a>
+            </li>
 
-        {{-- User --}}
+            <li class="menu-item {{ Request::is('dashboard/asn*') ? 'active' : '' }}">
+                <a href="/dashboard/asn" class="menu-link">
+                    <i class="fa-duotone fa-school fa-sm me-3"></i>
+                    <div data-i18n="Analytics">Asn</div>
+                </a>
+            </li>
 
-        <li class="menu-item {{ Request::is('dashboard/sppd*') ? 'active' : '' }}">
-            <a href="/dashboard/sppd" class="menu-link">
-                <i class="fa-duotone fa-note-sticky fa-sm me-3"></i>
-                <div data-i18n="Analytics">SPPD</div>
-            </a>
-        </li>
+            <li class="menu-item {{ Request::is('dashboard/golongan*') ? 'active' : '' }}">
+                <a href="/dashboard/golongan" class="menu-link">
+                    <i class="fa-duotone fa-stairs fa-sm me-3"></i>
+                    <div data-i18n="Analytics">Golongan</div>
+                </a>
+            </li>
 
-        <li class="menu-item {{ Request::is('dashboard/pegawai*') ? 'active' : '' }}">
-            <a href="/dashboard/pegawai" class="menu-link">
-                <i class="fa-duotone fa-user-tie fa-sm me-3"></i>
-                <div data-i18n="Analytics">Pegawai</div>
-            </a>
-        </li>
+            <li class="menu-item {{ Request::is('dashboard/jenis*') ? 'active' : '' }}">
+                <a href="/dashboard/jenis" class="menu-link">
+                    <i class="fa-duotone fa-list fa-sm me-3"></i>
+                    <div data-i18n="Analytics">Jenis Tugas</div>
+                </a>
+            </li>
 
-        <li class="menu-item {{ Request::is('dashboard/asn*') ? 'active' : '' }}">
-            <a href="/dashboard/asn" class="menu-link">
-                <i class="fa-duotone fa-school fa-sm me-3"></i>
-                <div data-i18n="Analytics">Asn</div>
-            </a>
-        </li>
+            <li class="menu-item {{ Request::is('dashboard/user*') ? 'active' : '' }}">
+                <a href="/dashboard/user" class="menu-link">
+                    <i class="fa-duotone fa-users fa-sm me-3"></i>
+                    <div data-i18n="Analytics">User</div>
+                </a>
+            </li>
+        @endif
 
-        <li class="menu-item {{ Request::is('dashboard/golongan*') ? 'active' : '' }}">
-            <a href="/dashboard/golongan" class="menu-link">
-                <i class="fa-duotone fa-stairs fa-sm me-3"></i>
-                <div data-i18n="Analytics">Golongan</div>
-            </a>
-        </li>
 
-        <li class="menu-item {{ Request::is('dashboard/jenis*') ? 'active' : '' }}">
-            <a href="/dashboard/jenis" class="menu-link">
-                <i class="fa-duotone fa-list fa-sm me-3"></i>
-                <div data-i18n="Analytics">Jenis Tugas</div>
-            </a>
-        </li>
-
-        <li class="menu-item {{ Request::is('dashboard/user*') ? 'active' : '' }}">
-            <a href="/dashboard/user" class="menu-link">
-                <i class="fa-duotone fa-users fa-sm me-3"></i>
-                <div data-i18n="Analytics">User</div>
-            </a>
-        </li>
 
     </ul>
 </aside>
