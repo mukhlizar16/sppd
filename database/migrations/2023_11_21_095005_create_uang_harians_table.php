80d3125a1eb90566d7fbbd283c5efa2c154ab2ef
mukhlizar16 <mukhlizaracer@gmail.com>
2023-12-31 23:59:31 +0700

    Suiting database migration

--- database/migrations/2023_11_21_095005_create_uang_harians_table.php
+++ database/migrations/2023_11_21_095005_create_uang_harians_table.php
@@ -4,24 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('uang_harians', function (Blueprint $table) {
+        Schema::create('uang_harian', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('sppd_id')->constrained('sppds')->onUpdate('cascade')->onDelete('restrict');
+            $table->foreignId('sppd_id');
             $table->integer('harian');
-            $table->integer('total_harian');
             $table->integer('konsumsi');
-            $table->integer('total_konsumsi');
             $table->integer('transportasi');
-            $table->integer('total_transportasi');
             $table->integer('representasi');
-            $table->integer('total_representasi');
             $table->timestamps();
         });
     }
@@ -31,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('uang_harians');
+        Schema::dropIfExists('uang_harian');
     }
 };
--- database/migrations/2023_11_21_095017_create_akomodasis_table.php
+++ database/migrations/2023_11_21_095017_create_akomodasis_table.php
@@ -4,26 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('akomodasis', function (Blueprint $table) {
+        Schema::create('akomodasi', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('sppd_id')->constrained('sppds')->onUpdate('cascade')->onDelete('restrict');
-            $table->string('name_hotel');
-            $table->date('tgl_masuk');
-            $table->date('tgl_keluar');
-            $table->string('no_invoice');
-            $table->string('no_kamar');
+            $table->foreignId('sppd_id')
+                ->index()
+                ->constrained('sppd')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->string('nama_hotel');
+            $table->date('check_in');
+            $table->date('check_out');
+            $table->string('nomor_invoice');
+            $table->string('nomor_kamar');
             $table->tinyInteger('lama_inap');
             $table->string('nama_kwitansi');
-            $table->integer('harga_permalam');
-            $table->integer('harga_permalam2');
-            $table->integer('total_uang');
+            $table->integer('harga');
+            $table->integer('harga_diskon');
             $table->integer('bbm');
             $table->string('dari');
             $table->string('ke');
@@ -36,6 +38,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('akomodasis');
+        Schema::dropIfExists('akomodasi');
     }
 };
